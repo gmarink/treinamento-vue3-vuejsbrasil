d7530e0d838bb70e3abadf039203f1ee6007e92f
Geraldo Marink <gmarink@gmail.com>
2021-04-21 23:39:32 -0400

    views e rotas

--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
  
 
-import Home = () => import('')
-import Feedbacks = () => import('')
-import Credentials = () => import('')
+const Home = () => import('../views/Home/index.vue')
+const Feedbacks = () => import('../views/Feedbacks/index.vue')
+const Credentials = () => import('../views/Credentials/index.vue')
 
 const routes = [
   {
