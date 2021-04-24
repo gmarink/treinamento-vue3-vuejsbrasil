ef91f8a1e003c354977c2fb8516c341d656e41b0
Geraldo Marink <gmarink@gmail.com>
2021-04-23 23:04:58 -0400

    criando tela incial

---
+++ dashboard/src/components/ModalFactory/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <teleport
+  to="body"
+  >
+  <div class="fixed top-0 left-0 z-50 flex items-center justify-center w-full bg-black bg-opacity-50"
+  >
+ <div class="fixed mx-10">
+ <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
+ <div class="flex flex-col px-12 py-10 bg-white"
+ >
+ <component :is="state.component" />
+ </div>
+ </div>
+ </div>
+  </div>
+  </teleport>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+export default {
+    setup () {
+        const state = reactive({
+            
+        })
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,13 +1,40 @@
 <template>
-  <h1 class="font-black  text-brand-main">home</h1>
+<div>
+  <custom-header
+    @create-accaount="handlerAccountCreate"
+    @login="handlerLogin"
+  />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker &copy; 2021</p>
+  </div>
+</div>
 </template>
 
 <script>
-export default {
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import Contact from './Contact.vue'
+import CustomHeader from './CustomHeader'
 
+export default {
+  components: { Contact, CustomHeader },
+  setup () {
+    const router = useRouter()
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedback' })
+      }
+    })
+    function handlerLogin () {
+    }
+    function handlerAccountCreate () {
+    }
+    return {
+      handlerLogin,
+      handlerAccountCreate
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
