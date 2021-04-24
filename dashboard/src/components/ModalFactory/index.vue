58367a5643298b5a140f8bf9df7c6acc5f19e93d
Geraldo Marink <gmarink@gmail.com>
2021-04-23 23:16:27 -0400

    modal factory 1

--- dashboard/src/components/ModalFactory/index.vue
+++ dashboard/src/components/ModalFactory/index.vue
@@ -2,7 +2,8 @@
   <teleport
   to="body"
   >
-  <div class="fixed top-0 left-0 z-50 flex items-center justify-center w-full bg-black bg-opacity-50"
+  <div v-if="state.isActive" class="fixed top-0 left-0 z-50 flex items-center justify-center w-full bg-black bg-opacity-50"
+  @click="handleModaltoogle({status: false})"
   >
  <div class="fixed mx-10">
  <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
@@ -17,14 +18,23 @@
 </template>
 
 <script>
-import { reactive } from '@vue/reactivity'
+import { reactive } from 'vue'
+const DEFAULT_WIDTH = 'w-3/4 lg:w-1/3'
 export default {
     setup () {
         const state = reactive({
-            
+            isActive: false, 
+            component: {},
+            props: {},
+            width: DEFAULT_WIDTH
         })
-    }
 
+        function handleModaltoogle ({ status }) {
+        }
+         return {
+        state
+    }
+    }
 }
 </script>
 
